aaa3ba3f7611786e34dce9b9e420652317b3eec9
Stefan <stefan@applied-ai.solutions>
2018-12-23 22:05:19 -0500

    review

--- 00_setup/conda-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# follow pyenv / virtualenv instructions: https://github.com/pyenv/pyenv-installer
-conda create -y -n bayes numpy scipy cython;
-conda activate bayes;
-conda config --add channels conda-forge;
-pip install git+https://github.com/theano/theano;
-pip install git+https://github.com/pymc-devs/pymc3;
-
