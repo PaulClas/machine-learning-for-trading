50d311487b6866470e4f01bb2830e0a1abf8d63b
Stefan Jansen <stefan@applied-ai.solutions>
2019-01-22 21:54:54 -0500

    Ch 16 complete

---
+++ 16_deep_learning/05_how_to_optimize_a_NN_architecture.py
@@ -0,0 +1,111 @@
+# coding: utf-8
+
+import warnings
+
+warnings.filterwarnings('ignore')
+
+import numpy as np
+import pandas as pd
+from joblib import dump
+
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
+
+import tensorflow as tf
+from keras.models import Sequential
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.layers import Dense, Dropout, Activation
+from keras.callbacks import EarlyStopping, TensorBoard
+
+np.random.seed(42)
+
+data = pd.read_hdf('data.h5', 'returns')
+features, label = data.drop('label', axis=1), data.label
+input_dim = features.shape[1]
+
+
+def auc_roc(y_true, y_pred):
+    # any tensorflow metric
+    value, update_op = tf.metrics.auc(y_true, y_pred)
+
+    # find all variables created for this metric
+    metric_vars = [i for i in tf.local_variables() if 'auc_roc' in i.name.split('/')[1]]
+
+    # Add metric variables to GLOBAL_VARIABLES collection.
+    # They will be initialized for new session.
+    for v in metric_vars:
+        tf.add_to_collection(tf.GraphKeys.GLOBAL_VARIABLES, v)
+
+    # force to update metric values
+    with tf.control_dependencies([update_op]):
+        value = tf.identity(value)
+        return value
+
+
+def make_model(dense_layers, activation, dropout):
+    '''Creates a multi-layer perceptron model
+    
+    dense_layers: List of layer sizes; one number per layer
+    '''
+
+    model = Sequential()
+    for i, layer_size in enumerate(dense_layers, 1):
+        if i == 1:
+            model.add(Dense(layer_size, input_dim=input_dim))
+            model.add(Activation(activation))
+        else:
+            model.add(Dense(layer_size))
+            model.add(Activation(activation))
+    model.add(Dropout(dropout))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='Adam',
+                  metrics=['binary_accuracy', auc_roc])
+
+    return model
+
+
+test_size, n_splits = .1, 5
+X_train, X_test, y_train, y_test = train_test_split(features, label,
+                                                    test_size=test_size,
+                                                    random_state=42,
+                                                    shuffle=True,
+                                                    stratify=data.label)
+
+clf = KerasClassifier(make_model, epochs=10, batch_size=32)
+
+cv = StratifiedKFold(n_splits=5, shuffle=True)
+
+param_grid = {'dense_layers': [[32], [32, 32], [64], [64, 64], [64, 64, 32], [64, 32], [128]],
+              'activation'  : ['relu', 'tanh'],
+              'dropout'     : [.25, .5, .75],
+              }
+
+gs = GridSearchCV(estimator=clf,
+                  param_grid=param_grid,
+                  scoring='roc_auc',
+                  cv=cv,
+                  refit=True,
+                  return_train_score=True,
+                  n_jobs=-1,
+                  verbose=1,
+                  error_score=np.nan
+                  )
+
+fit_params = dict(callbacks=[EarlyStopping(monitor='auc_roc', patience=300, verbose=1, mode='max')],
+                  verbose=2,
+                  epochs=50)
+
+gs.fit(X=X_train.astype(float), y=y_train, **fit_params)
+print('\nBest Score: {:.2%}'.format(gs.best_score_))
+print('Best Params:\n', pd.Series(gs.best_params_))
+
+dump(gs, 'gs.joblib')
+gs.best_estimator_.model.save('best_model.h5')
+
+with pd.HDFStore('data.h5') as store:
+    store.put('X_train', X_train)
+    store.put('X_test', X_test)
+    store.put('y_train', y_train)
+    store.put('y_test', y_test)
---
+++ 16_deep_learning/README.md
@@ -0,0 +1,43 @@
+# Chapter 16: Deep Learning
+
+## How Deep Learning Works
+
+- [Deep Learning](https://www.deeplearningbook.org/), Ian Goodfellow, Yoshua Bengio and Aaron Courville, MIT Press, 2016
+- [Deep learning](https://www.nature.com/articles/nature14539), Yann LeCun, Yoshua Bengio, and Geoffrey Hinton, Nature 2015
+- [Neural Networks and Deep Learning](http://neuralnetworksanddeeplearning.com/), Michael A. Nielsen, Determination Press, 2015
+- [The Quest for Artificial Intelligence - A History of Ideas and Achievements](https://ai.stanford.edu/~nilsson/QAI/qai.pdf), Nils J. Nilsson, Cambridge University Press, 2010
+- [One Hundred Year Study on Artificial Intelligence (AI100)](https://ai100.stanford.edu/)
+
+
+### Backpropagation
+
+- [Gradient Checking & Advanced Optimization](http://ufldl.stanford.edu/wiki/index.php/Gradient_checking_and_advanced_optimization), Unsupervised Feature Learning and Deep Learning, Stanford University
+- [An overview of gradient descent optimization algorithms](http://ruder.io/optimizing-gradient-descent/index.html#momentum), Sebastian Ruder, 2016
+
+
+
+## Popular Deep Learning libraries
+
+- [Microsoft Cognitive Toolkit (CNTK)](https://github.com/Microsoft/CNTK)
+- [Caffe](http://caffe.berkeleyvision.org/)
+- [Thenao](http://www.deeplearning.net/software/theano/), developed at University of Montreal since 2007
+- [Apache MXNet](https://mxnet.apache.org/), used by Amazon
+- [Chainer](https://chainer.org/), developed by the Japanese company Preferred Networks
+- [Torch](http://torch.ch/), uses Lua, basis for PyTorch
+- [Deeplearning4J](https://deeplearning4j.org/), uses Java
+### How to use Keras
+
+- [Keras documentation](https://keras.io/)
+
+### How to use Tensorboard
+
+- [TensorBoard: Visualizing Learning](https://www.tensorflow.org/guide/summaries_and_tensorboard)
+
+### How to use PyTorch
+
+- [PyTorch Documentation](https://pytorch.org/docs)
+- [PyTorch Tutorials](https://pytorch.org/tutorials)
+- [PyTorch Ecosystem](https://pytorch.org/ecosystem)
+    - [AllenNLP](https://allennlp.org/), state-of-the-art NLP platform developed by the Allen Institute for Artificial Intelligence
+    - [Flair](https://github.com/zalandoresearch/flair),  simple framework for state-of-the-art NLP developed at Zalando
+    - [fst.ai](http://www.fast.ai/), simplifies training NN using modern best practices; offers online training
